16b975d7f6486d450922fce593d8854bad1bf3b4
Igor Nemkovich <igor.nemkovich@capital.com>
2025-02-25 19:43:41 +0100

    Add LeafNode class with to_html method and corresponding tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,15 @@ class HTMLNode:
   
   def __repr__(self):
     return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+  
+
+class LeafNode(HTMLNode):
+  def __init__(self, tag, value, props=None):
+    super().__init__(tag, value, props=props)
+
+  def to_html(self):
+    if self.value is None:
+      raise ValueError("LeafNode value cannot be None")
+    if self.tag is None or self.tag == "":
+      return self.value
+    return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -47,6 +47,45 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             node.__repr__(),
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+        self.assertEqual(
+            node.tag,
+            "a",
+        )
+        self.assertEqual(
+            node.value,
+            "Click me!",
+        )
+        self.assertEqual(
+            node.props,
+            {"href": "https://www.google.com"},
+        )
+
+    def test_values(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(
+            node.tag,
+            "p",
+        )
+        self.assertEqual(
+            node.value,
+            "This is a paragraph of text.",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
         )
 
 
